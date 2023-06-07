497e64ebf00f8bb1095354f080905e66242eec8b
Thanh Tran <thanh.tranviet@teragroup.io>
2023-06-07 13:56:56 +0700

    add env

--- main.go
+++ main.go
@@ -1,109 +1,109 @@
 package main
 
 import (
-  "net/http"
-  "github.com/gin-gonic/gin"
-  "github.com/golang-jwt/jwt/v4"
-  "time"
-  "encoding/pem"
-  "crypto/x509"
-  "os"
-  "math/rand"
+	"crypto/x509"
+	"encoding/pem"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
 )
 
 type Profile struct {
-	Id string `json:"id"`
+	Id          string `json:"id"`
 	PhoneNumber string `json:"phoneNumber"`
-	FirstName string `json:"firstName"`
-	LastName string `json:"lastName"`
-	Email string `json:"email"` 
-	CreatedBy string `json:"createdBy"`
-	UpdatedBy string `json:"updatedBy"` 
-	CreatedAt int `json:"createdAt"` 
-	UpdatedAt int `json:"updatedAt"`
+	FirstName   string `json:"firstName"`
+	LastName    string `json:"lastName"`
+	Email       string `json:"email"`
+	CreatedBy   string `json:"createdBy"`
+	UpdatedBy   string `json:"updatedBy"`
+	CreatedAt   int    `json:"createdAt"`
+	UpdatedAt   int    `json:"updatedAt"`
 }
 
 type ClaimsInfo struct {
 	Profile Profile `json:"profile"`
-	Aud string `json:"aud"`
-	Iss string `json:"iss"`
-	Sub string `json:"sub"`
-	Iat int `json:"iat"`
-	Exp int `json:"exp"`
-} 
+	Aud     string  `json:"aud"`
+	Iss     string  `json:"iss"`
+	Sub     string  `json:"sub"`
+	Iat     int     `json:"iat"`
+	Exp     int     `json:"exp"`
+}
 
 type InteractiveBody struct {
 	// json tag to de-serialize json body
-	Profiles []ClaimsInfo `json:"profiles"`
-	SecretKey string `json:"secretKey"`
+	Profiles  []ClaimsInfo `json:"profiles"`
+	SecretKey string       `json:"secretKey"`
 }
 
 type Body struct {
 	// json tag to de-serialize json body
-	AppName string `json:"appName"`
+	AppName       string   `json:"appName"`
 	AppUserIdList []string `json:"AppUserIdList"`
 	ContentIdList []string `json:"ContentIdList"`
-	SecretKey string `json:"secretKey"`
+	SecretKey     string   `json:"secretKey"`
 }
 
 func main() {
-  	r := gin.Default()
+	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-		"message": "Hello world",
+			"message": "Hello world",
 		})
 	})
 
 	r.GET("/ping", func(c *gin.Context) {
 		srvName := os.Getenv("SERVICE_NAME")
 		c.JSON(http.StatusOK, gin.H{
-		"message": srvName,
+			"message": srvName,
 		})
 	})
 
 	r.POST("/generate-token", func(c *gin.Context) {
-		body:=Body{}
-		if err:=c.BindJSON(&body); err!=nil {
-			c.AbortWithError(http.StatusBadRequest,err)
-		return
+		body := Body{}
+		if err := c.BindJSON(&body); err != nil {
+			c.AbortWithError(http.StatusBadRequest, err)
+			return
 		}
 		tokenList := []string{}
 		for i := 0; i < len(body.AppUserIdList); i++ {
-			token, _ := generateJWT(body.AppName, body.AppUserIdList[i], body.ContentIdList[rand.Intn(len(body.ContentIdList))], body.SecretKey)
+			token, _ := generateJWT(body.AppName, body.AppUserIdList[i], body.ContentIdList[rand.Intn(len(body.ContentIdList))], os.Getenv("GENERATOR_KEY"))
 			tokenList = append(tokenList, token)
 		}
 
 		c.JSON(http.StatusOK, gin.H{
-		"token": tokenList,
+			"token": tokenList,
 		})
 	})
 
 	r.POST("/generate-token-to-interactive", func(c *gin.Context) {
-		body:=InteractiveBody{}
-		if err:=c.BindJSON(&body); err!=nil {
-			c.AbortWithError(http.StatusBadRequest,err)
-		return
+		body := InteractiveBody{}
+		if err := c.BindJSON(&body); err != nil {
+			c.AbortWithError(http.StatusBadRequest, err)
+			return
 		}
 		tokenList := []string{}
 		for i := 0; i < len(body.Profiles); i++ {
-			token, _ := generateJWTToInteractive(body.Profiles[i], body.SecretKey)
+			token, _ := generateJWTToInteractive(body.Profiles[i], os.Getenv("GENERATOR_INTERACTIVE_KEY"))
 			tokenList = append(tokenList, token)
 		}
 
 		c.JSON(http.StatusOK, gin.H{
-		"token": tokenList,
-    })
-  })
-  r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+			"token": tokenList,
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
 func generateJWT(appName string, appUserId string, contentId string, secretKey string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodES256, jwt.MapClaims{
-		"exp": time.Now().AddDate(2, 0, 0).Unix(),
-		"app_name": appName,
+		"exp":         time.Now().AddDate(2, 0, 0).Unix(),
+		"app_name":    appName,
 		"app_user_id": appUserId,
-		"content_id":contentId,
+		"content_id":  contentId,
 	})
 	block, _ := pem.Decode([]byte(secretKey))
 	key, _ := x509.ParsePKCS8PrivateKey(block.Bytes)
@@ -119,15 +119,15 @@ func generateJWT(appName string, appUserId string, contentId string, secretKey s
 func generateJWTToInteractive(claimsInfo ClaimsInfo, secretKey string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodES256, jwt.MapClaims{
 		"profile": jwt.MapClaims{
-			"id": claimsInfo.Profile.Id,
+			"id":          claimsInfo.Profile.Id,
 			"phoneNumber": claimsInfo.Profile.PhoneNumber,
-			"firstName": claimsInfo.Profile.FirstName,
-			"lastName": claimsInfo.Profile.LastName,
-			"email": claimsInfo.Profile.Email,
-			"createdBy": claimsInfo.Profile.CreatedBy,
-			"updatedBy": claimsInfo.Profile.UpdatedBy,
-			"createdAt": claimsInfo.Profile.CreatedAt,
-			"updatedAt": claimsInfo.Profile.UpdatedAt,
+			"firstName":   claimsInfo.Profile.FirstName,
+			"lastName":    claimsInfo.Profile.LastName,
+			"email":       claimsInfo.Profile.Email,
+			"createdBy":   claimsInfo.Profile.CreatedBy,
+			"updatedBy":   claimsInfo.Profile.UpdatedBy,
+			"createdAt":   claimsInfo.Profile.CreatedAt,
+			"updatedAt":   claimsInfo.Profile.UpdatedAt,
 		},
 		"aud": claimsInfo.Aud,
 		"iss": claimsInfo.Iss,
@@ -145,4 +145,3 @@ func generateJWTToInteractive(claimsInfo ClaimsInfo, secretKey string) (string, 
 
 	return tokenString, err
 }
-
